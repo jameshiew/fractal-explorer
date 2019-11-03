2d6462a25804ceb7caff1ecae341c8460781bb4f
James Hiew <james@hiew.net>
2019-11-03 16:52:10 +0000

    Add a viewport

--- fractal/fractal.go
+++ fractal/fractal.go
@@ -1,23 +1,55 @@
 package fractal
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/widget"
+	"image/color"
 )
 
 const title = "Fractal Explorer"
+
+type viewport struct {
+	window fyne.Window
+	canvas fyne.CanvasObject
+}
+
+func (v *viewport) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	v.canvas.Resize(size)
+}
+
+func (v *viewport) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	return fyne.NewSize(320, 240)
+}
+
+func stubPixelColor(px, py, w, h int) color.Color {
+	return color.RGBA{
+		R: 255,
+		G: 100,
+		B: 100,
+		A: 255,
+	}
+}
 
 // Run runs the application
 func Run() {
 	app := app.New()
 
 	w := app.NewWindow(title)
+	vp := &viewport{
+		window: w,
+		canvas: canvas.NewRasterWithPixels(stubPixelColor),
+	}
 	w.SetContent(widget.NewVBox(
 		widget.NewLabel("Hello Fyne!"),
+		fyne.NewContainerWithLayout(vp, vp.canvas),
 		widget.NewButton("Quit", func() {
 			app.Quit()
 		}),
 	))
+
+	vp.window.Canvas().Refresh(vp.canvas)
 
 	w.ShowAndRun()
 }
