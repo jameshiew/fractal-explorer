692b8501f6177f5be6b321f72f236d976e4b8785
James Hiew <james@hiew.net>
2019-11-03 18:39:48 +0000

    Add rudimentary zooming

--- fractal/fractal.go
+++ fractal/fractal.go
@@ -1,7 +1,9 @@
 package fractal
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 )
 
 const (
@@ -17,5 +19,18 @@ func Run() {
 	w := app.NewWindow(title)
 	cnvs := newFractalCanvas()
 	w.SetContent(&cnvs)
+	w.Canvas().SetOnTypedKey(func(event *fyne.KeyEvent) {
+		const zoomFactor = 1.1
+		switch event.Name {
+		case fyne.KeyQ:
+			cnvs.drawer.scale.x /= zoomFactor
+			cnvs.drawer.scale.y /= zoomFactor
+			widget.Refresh(&cnvs)
+		case fyne.KeyW:
+			cnvs.drawer.scale.x *= zoomFactor
+			cnvs.drawer.scale.y *= zoomFactor
+			widget.Refresh(&cnvs)
+		}
+	})
 	w.ShowAndRun()
 }
