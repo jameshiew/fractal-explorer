14c71b973c5d1d2957aada01edd67179e40cf74a
James Hiew <james@hiew.net>
2019-11-03 16:44:37 +0000

    Factor out title

--- fractal/fractal.go
+++ fractal/fractal.go
@@ -5,11 +5,13 @@ import (
 	"fyne.io/fyne/widget"
 )
 
+const title = "Fractal Explorer"
+
 // Run runs the application
 func Run() {
 	app := app.New()
 
-	w := app.NewWindow("Hello")
+	w := app.NewWindow(title)
 	w.SetContent(widget.NewVBox(
 		widget.NewLabel("Hello Fyne!"),
 		widget.NewButton("Quit", func() {
