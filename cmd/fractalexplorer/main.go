b8fbf63b6adbb7a5cdec62f61fc2d16da6512f5a
James Hiew <james@hiew.net>
2019-11-12 22:03:50 +0000

    Set a backend and formatter for logger

--- cmd/fractalexplorer/main.go
+++ cmd/fractalexplorer/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/op/go-logging"
 
 	"gitlab.com/jameshiew/fractal-explorer/internal/gui"
@@ -9,6 +11,8 @@ import (
 const title = "Fractal Explorer"
 
 func main() {
+	logging.SetBackend(logging.NewLogBackend(os.Stdout, "", 0))
+	logging.SetFormatter(logging.MustStringFormatter(`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`))
 	log := logging.MustGetLogger(title)
 
 	log.Info("Starting up")
