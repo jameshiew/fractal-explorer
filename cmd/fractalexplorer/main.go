5c7efda94c05b6997ec4e9179c0c141e9f62b1a2
James Hiew <james@hiew.net>
2019-11-12 21:10:12 +0000

    log at start up and shut down

--- cmd/fractalexplorer/main.go
+++ cmd/fractalexplorer/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"gitlab.com/jameshiew/fractal-explorer/internal/gui"
 )
 
 const title = "Fractal Explorer"
 
 func main() {
+	log.Println("Starting up")
 	gui.Run(title)
+	log.Println("Shutting down")
 }
